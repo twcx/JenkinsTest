01335145401d9233a25df842fd8ca52243899bfc
杨海龙 <yanghailong@alipaycampus.com>
2019-04-11 18:00:34 +0800

    NEW 初始化测试项目

---
+++ JenkinsTest/JenkinsTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JenkinsTest
+//
+//  Created by QuDian on 2019/4/11.
+//  Copyright © 2019 yanghailong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
